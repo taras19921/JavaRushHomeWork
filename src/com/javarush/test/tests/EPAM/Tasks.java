d8c8a5e0392479ce9eb4814e51e1cfdfb70c24cf
taras19921 <taras19921@gmail.com>
2015-07-16 08:47:48 +0300

    new changes

--- src/com/javarush/test/tests/EPAM/Tasks.java
+++ src/com/javarush/test/tests/EPAM/Tasks.java
@@ -10,9 +10,40 @@ import java.util.ArrayList;
  */
 public class Tasks
 {
-    /*
-    // reverseNumber
     public static void main(String[] args) throws IOException
+    {
+        //fibonacci(8);
+        //fibonacci3Number(8);
+        //factorial(7);
+        //sortArray();
+        reverseNumber();
+
+    }
+
+    // Natural number
+    public static void naturalNumber(int n)
+    {
+        ArrayList<Integer> natural = new ArrayList<>();
+        int i = 1;
+        label1: do
+        {
+            for (int j = 2; j < i; j++)
+            {
+                if ((i % j == 0))
+                {
+                    i++;
+                    continue label1;
+                }
+            }
+            natural.add(i);
+            i++;
+        } while (natural.size() < n);
+        System.out.println(natural);
+
+    }
+
+    // reverseNumber
+    public static void reverseNumber() throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //Integer a = Integer.parseInt(br.readLine());
@@ -21,73 +52,75 @@ public class Tasks
         s.reverse();
         System.out.println(s);
     }
-    */
 
-    /*
-    // Natural number
-    public static void main(String[] args) throws IOException
-    {
-        ArrayList<Integer> natural = new ArrayList<>();
-        int i = 1;
-        label1: do
-        {
-
-                for (int j = 2; j < i; j++)
-                {
-                    if ((i % j == 0))
-                    {
-                        i++;
-                        continue label1;
-                    }
+    //Sorted array
+    public static void sortArray() {
+        int[] mas = {4, 2, 34, 22, 44, 1, 16, 55, 3, 8};
+        int max = 0;
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = i + 1; j < mas.length ; j++) {
+                if (mas[j] < mas[i]) {
+                    max = mas[i];
+                    mas[i] = mas[j];
+                    mas[j] = max;
                 }
-                natural.add(i);
-                i++;
-        } while (natural.size() < 30);
-        System.out.println(natural);
-
-    }
-    */
-
-    // Numbers of Fibonacci && Factorization
-    public static void main(String[] args) throws IOException
-    {
-        Tasks tasks = new Tasks();
-        //tasks.fibonacci(9);
-        tasks.factorial(5);
-        //System.out.println(tasks.fibonacci1(9));     //34
-        //System.out.println(tasks.fibonacci1(5));     //5
-        //System.out.println(tasks.fibonacci1(2));     //1
+            }
+        }
+        for (int i = 0; i < mas.length; i++)
+            System.out.print(mas[i] + " ");
     }
 
-    public void fibonacci(int n) {
+    //Fibonacci
+    //1 2 3 5 8 13 21 34 55
+    public static void fibonacci(int n) {
         //int[] mas = new int[n];
         int a = 0;
         int b = 1;
 
         for (int i = 0; i < n; i++)
         {
-            int c = a + b;
-            System.out.println(c);
-            //mas[i] = c;
+            int fact = a + b;
+            System.out.print(fact + " ");
+            //mas[i] = fact;
             a = b;
-            b = c;
+            b = fact;
         }
+        System.out.println();
         /*for (int i = 0; i < mas.length; i++)
             System.out.println(mas[i]);
             */
     }
 
-    public void factorial(int n)
+    //Factorial
+    public static void factorial(int n)
     {
-        int c = 1;
+        int fact = 1;
         for (int i = 0; i < n; i++)
         {
-            c = c * (i + 1);
-            System.out.println(c);
+            fact = fact * (i + 1);
+            System.out.print(fact + " ");
         }
         //System.out.println(c);
     }
 
+    //Fibonacci
+    public static void fibonacci3Number(int n)
+    {
+        //int[] mas = new int[n];
+        int a = 0;
+        int b = 1;
+        int c = 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            int d = a + b + c;
+            System.out.print(d + " ");
+            //mas[i] = c;
+            a = b;
+            b = c;
+            c = d;
+        }
+    }
         public int fibonacci1(int n) {
         if (n < 3)
             return 1;
